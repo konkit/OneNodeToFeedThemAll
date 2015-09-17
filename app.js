8946abe4016ec9a9428ed2e708f0a3e1dfdf0cd2
Lukasz Tenerowicz <lukasz.tenerowicz@polcode.net>
2015-09-17 12:51:25 +0200

    Added jade views

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 var express = require('express');
 var passport = require('passport');
 var util = require('util');
+var path = require('path');
+
+var session = require('express-session')
+sessionStore = new session.MemoryStore;
 
 var app = express();
 // Express middleware
 app.use(require('morgan')('combined'));
 app.use(require('cookie-parser')());
 app.use(require('body-parser').urlencoded({ extended: true }));
-app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+app.use(session({ store: sessionStore, secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Initialize Passport session
 app.use(passport.initialize());
@@ -21,12 +26,23 @@ passport.deserializeUser(function(user, done) {
   done(null, user);
 });
 
+
+
+// Init routes
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+app.use('/', routes);
+app.use('/users', users);
+
+// Init views
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// Add handling FB and Twitter
 facebook = require('./facebook.js')(app, passport);
 twitter = require('./twitter.js')(app, passport);
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
-});
+
+module.exports = app;

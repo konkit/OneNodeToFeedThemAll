37570749c263b65da2e8359c0feda068de8c11b3
Lukasz Tenerowicz <lukasz.tenerowicz@polcode.net>
2015-09-23 11:55:53 +0200

    Added read stream permission

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -1,6 +1,6 @@
 module.exports = function(app, passport) {
 
-  app.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
+  app.get('/auth/facebook', passport.authenticate('facebook', { scope : ['email', 'read_stream'] }));
   app.get('/auth/facebook/callback',
     passport.authenticate('facebook', {
       successRedirect : '/profile',
@@ -10,13 +10,6 @@ module.exports = function(app, passport) {
   app.get('/auth/twitter', passport.authenticate('twitter', { scope : 'email' }));
   app.get('/auth/twitter/callback',
     passport.authenticate('twitter', {
-      successRedirect : '/profile',
-      failureRedirect : '/'
-    }));
-
-  app.get('/auth/google', passport.authenticate('google', { scope : ['profile', 'email'] }));
-  app.get('/auth/google/callback',
-    passport.authenticate('google', {
       successRedirect : '/profile',
       failureRedirect : '/'
     }));

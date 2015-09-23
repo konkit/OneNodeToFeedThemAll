911ef37562b7935dc9771013a8bfc1a1ac7d840d
Lukasz Tenerowicz <lukasz.tenerowicz@polcode.net>
2015-09-23 11:08:55 +0200

    Added MongoDB env to DB settings

--- config/database.js
+++ config/database.js
@@ -2,5 +2,5 @@ var User       = require('../app/models/user');
 var Post 			 = require('../app/models/post');
 
 module.exports = {
-	'url' : 'mongodb://localhost/test'
+	'url' : process.env.MONGOLAB_URI || 'mongodb://localhost/test'
 };

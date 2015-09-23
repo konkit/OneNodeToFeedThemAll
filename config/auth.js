5fad8b6b6fc3817873646f03a03d0d3cb067a92f
Lukasz Tenerowicz <lukasz.tenerowicz@polcode.net>
2015-09-23 11:20:26 +0200

    Added env variables for server_url

--- config/auth.js
+++ config/auth.js
@@ -2,11 +2,11 @@ module.exports = {
 	'facebookAuth' : {
 		'clientID' 		 : '198776883637263',
 		'clientSecret' : '77b1d915639b2c2b6c08db5d7b011cbb',
-		'callbackURL'  : 'http://localhost:3000/auth/facebook/callback'
+		'callbackURL'  : ( process.env.SERVER_URL || 'http://localhost:3000') + '/auth/facebook/callback'
 	},
 	'twitterAuth' : {
 		'consumerKey' 		: '4qf5IouYYD554s5PN02pdMui7',
 		'consumerSecret' 	: 'QEwsLu0IN8QJlWAsT1AfFE5uG5c9ie6AERnMOmSIHTENYhg5bY',
-		'callbackURL' 		: 'http://localhost:3000/auth/twitter/callback'
+		'callbackURL' 		: ( process.env.SERVER_URL || 'http://localhost:3000') + '/auth/twitter/callback'
 	}
 };

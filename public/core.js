9eba2b79efd5914c947849428333dffc43a054be
Lukasz Tenerowicz <lukasz.tenerowicz@polcode.net>
2015-09-29 13:34:04 +0200

    Added spinner

--- public/core.js
+++ public/core.js
@@ -1,35 +1,44 @@
 // public/core.js
 var oneNodeToFeedThemAll = angular.module('oneNodeToFeedThemAll',
   [
-    'ngSanitize'
+    'ngSanitize',
+    'angularSpinner'
   ]
 );
 
-oneNodeToFeedThemAll.controller('mainController', ['$scope', '$http', function($scope, $http) {
-  $scope.toggleFacebook = true;
-  $scope.toggleTwitter  = true;
+oneNodeToFeedThemAll.controller('mainController',
+[
+  '$scope', '$http', 'usSpinnerService',
+  function($scope, $http, usSpinnerService) {
+    $scope.toggleFacebook = true;
+    $scope.toggleTwitter  = true;
 
-  $scope.updateFeeds = function() {
-    $http({
-      url: '/api/feeds',
-      method: 'GET',
-      params: {
-        fb: $scope.toggleFacebook,
-        tw: $scope.toggleTwitter
-      }
-    }).success(function(data) {
-      console.log(data);
-      $scope.feedData = data;
-      console.log(data);
-    })
-    .error(function(data) {
-      console.log('Error: ' + data);
-    });
-  }
+    $scope.updateFeeds = function() {
+      usSpinnerService.spin('spinner-1');
 
-  $scope.updateFeeds($scope, $http);
+      $http({
+        url: '/api/feeds',
+        method: 'GET',
+        params: {
+          fb: $scope.toggleFacebook,
+          tw: $scope.toggleTwitter
+        }
+      }).success(function(data) {
+        usSpinnerService.stop('spinner-1');
+        console.log(data);
+        $scope.feedData = data;
+        console.log(data);
+      })
+      .error(function(data) {
+        usSpinnerService.stop('spinner-1');
+        console.log('Error: ' + data);
+      });
+    }
 
-  setInterval(function() {
     $scope.updateFeeds($scope, $http);
-  }, 10000);
-}]);
+
+    setInterval(function() {
+      $scope.updateFeeds($scope, $http);
+    }, 10000);
+  }
+]);
